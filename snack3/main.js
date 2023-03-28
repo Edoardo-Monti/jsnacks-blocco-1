20489e6915ae73b2078591658a94ca5b0a87bc93
Your Namede <edoardo.monti.bo@gmail.com>
2023-03-28 11:55:12 +0200

    fine degli snack

---
+++ snack3/main.js
@@ -0,0 +1,14 @@
+const numero1 = parseInt(prompt(`inserisci un numero`))
+const numero2 = parseInt(prompt(`inserisci un numero`))
+const numero3 = parseInt(prompt(`inserisci un numero`))
+const numero4 = parseInt(prompt(`inserisci un numero`))
+const numero5 = parseInt(prompt(`inserisci un numero`))
+const numero6 = parseInt(prompt(`inserisci un numero`))
+const numero7 = parseInt(prompt(`inserisci un numero`))
+const numero8 = parseInt(prompt(`inserisci un numero`))
+const numero9 = parseInt(prompt(`inserisci un numero`))
+const numero10 = parseInt(prompt(`inserisci un numero`))
+
+let somma = numero1 + numero2 + numero3 + numero4 + numero5 + numero6 + numero7 + numero8 + numero9 + numero10
+
+console.log(`${somma}`)
---
+++ snack4/main.js
@@ -0,0 +1,9 @@
+const array = []
+
+for(let i = 0; i<= 6; i++) {
+    const numero = parseInt(prompt(`numero`))
+    if(numero % 2 != 0 ){
+        array.push(numero)
+        console.log(array)
+    }
+}
