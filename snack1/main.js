80c89e74881355ac194db63b0a8e3df322e753a0
Your Namede <edoardo.monti.bo@gmail.com>
2023-03-28 10:58:08 +0200

    snack 1

---
+++ snack1/main.js
@@ -0,0 +1,10 @@
+let const1 = prompt(`numero1`)
+let const2 = prompt(`numero2`)
+
+if(const1 > const2){
+    console.log(const1)
+}else if(const1 == const2){
+    console.log(const2, const1)
+}else{
+    console.log(const2)
+}
